c6ee6a0cba1400e0d4859d5e53e5a6032b3c6ef6
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-09-14 17:26:27 +0900

    fix: [남현기] 딥러닝 모델과 소켓 클라이언트를 Local 서버에 구성합니다 [MUSE-I-11]

---
+++ deep_learn/emotion/emotion.py
@@ -0,0 +1,194 @@
+from kobert_tokenizer import KoBERTTokenizer
+from transformers import BertModel
+from transformers import AdamW
+from transformers.optimization import get_cosine_schedule_with_warmup
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import gluonnlp as nlp
+import numpy as np
+import pandas as pd
+from tqdm.notebook import tqdm
+from tqdm import tqdm, tqdm_notebook
+import re
+
+tokenizer = KoBERTTokenizer.from_pretrained('skt/kobert-base-v1')
+bertmodel = BertModel.from_pretrained('skt/kobert-base-v1', return_dict=False)
+vocab = nlp.vocab.BERTVocab.from_sentencepiece(tokenizer.vocab_file, padding_token='[PAD]')
+
+tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
+device = torch.device("cpu")
+
+# parameter 값 출처 : https://github.com/SKTBrain/KoBERT/blob/master/scripts/NSMC/naver_review_classifications_pytorch_kobert.ipynb
+max_len = 64
+batch_size = 128
+warmup_ratio = 0.1
+num_epochs = 5
+max_grad_norm = 1
+log_interval = 200
+learning_rate = 5e-5
+
+
+class BERTClassifier(nn.Module):
+    def __init__(self,
+                 bert,
+                 hidden_size=768,
+                 num_classes=7,  # 감정 클래스 수로 조정
+                 dr_rate=None,
+                 params=None):
+        super(BERTClassifier, self).__init__()
+        self.bert = bert
+        self.dr_rate = dr_rate
+
+        self.classifier = nn.Linear(hidden_size, num_classes)
+        if dr_rate:
+            self.dropout = nn.Dropout(p=dr_rate)
+
+    def gen_attention_mask(self, token_ids, valid_length):
+        attention_mask = torch.zeros_like(token_ids)
+        for i, v in enumerate(valid_length):
+            attention_mask[i][:v] = 1
+        return attention_mask.float()
+
+    def forward(self, token_ids, valid_length, segment_ids):
+        attention_mask = self.gen_attention_mask(token_ids, valid_length)
+
+        _, pooler = self.bert(input_ids=token_ids, token_type_ids=segment_ids.long(),
+                              attention_mask=attention_mask.float().to(token_ids.device), return_dict=False)
+        if self.dr_rate:
+            out = self.dropout(pooler)
+        return self.classifier(out)
+
+
+class BERTSentenceTransform:
+    def __init__(self, tokenizer, max_seq_length, vocab, pad=True, pair=True):
+        self._tokenizer = tokenizer
+        self._max_seq_length = max_seq_length
+        self._pad = pad
+        self._pair = pair
+        self._vocab = vocab
+
+    def __call__(self, line):
+        text_a = line[0]
+        if self._pair:
+            assert len(line) == 2
+            text_b = line[1]
+
+        print(f"text_a: {text_a}")
+        print(f"text_a[0]: {text_a[0]}")
+        tokens_a = self._tokenizer.tokenize(text_a[0])
+        tokens_b = None
+
+        if self._pair:
+            tokens_b = self._tokenizer(text_b)
+
+        if tokens_b:
+            self._truncate_seq_pair(tokens_a, tokens_b,
+                                    self._max_seq_length - 3)
+        else:
+            if len(tokens_a) > self._max_seq_length - 2:
+                tokens_a = tokens_a[0:(self._max_seq_length - 2)]
+
+        vocab = self._vocab
+        tokens = []
+        tokens.append(vocab.cls_token)
+        tokens.extend(tokens_a)
+        tokens.append(vocab.sep_token)
+        segment_ids = [0] * len(tokens)
+
+        if tokens_b:
+            tokens.extend(tokens_b)
+            tokens.append(vocab.sep_token)
+            segment_ids.extend([1] * (len(tokens) - len(segment_ids)))
+
+        input_ids = self._tokenizer.convert_tokens_to_ids(tokens)
+
+        # The valid length of sentences. Only real  tokens are attended to.
+        valid_length = len(input_ids)
+
+        if self._pad:
+            # Zero-pad up to the sequence length.
+            padding_length = self._max_seq_length - valid_length
+            # use padding tokens for the rest
+            input_ids.extend([vocab[vocab.padding_token]] * padding_length)
+            segment_ids.extend([0] * padding_length)
+
+        return np.array(input_ids, dtype='int32'), np.array(valid_length, dtype='int32'), \
+            np.array(segment_ids, dtype='int32')
+
+
+class BERTDataset(Dataset):
+    def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, vocab, max_len,
+                 pad, pair):
+        transform = BERTSentenceTransform(
+            tokenizer, max_seq_length=max_len, vocab=vocab, pad=pad, pair=pair)
+
+        self.sentences = [transform([i[sent_idx]]) for i in dataset]
+        self.labels = [np.int32(i[label_idx]) for i in dataset]
+
+    def __getitem__(self, i):
+        return self.sentences[i] + (self.labels[i],)
+
+    def __len__(self):
+        return len(self.labels)
+
+
+import os
+
+script_directory = os.path.dirname(__file__)
+print("emotion script_directory: ", script_directory)
+absolute_module_path = os.path.abspath(os.path.join(script_directory, '../../data/model.pt'))
+print("absolute_module_path: ", absolute_module_path)
+relative_module_path = os.path.relpath(absolute_module_path, os.path.abspath(os.getcwd()))
+print("relative_module_path: ", relative_module_path)
+
+model = BERTClassifier(bertmodel, dr_rate=0.5).to(device)
+
+model.load_state_dict(torch.load(
+    os.path.join(script_directory, '../../data/model.pt'), map_location=device))
+
+
+def sentiment_analysis(data):
+    sentence = [data, '0']
+    dataset_another = [sentence]
+
+    another_test = BERTDataset(dataset_another, 0, 1, tok, vocab, max_len, True, False)  # 토큰화한 문장
+    test_dataloader = torch.utils.data.DataLoader(another_test, batch_size=batch_size, num_workers=5)  # torch 형식 변환
+
+    model.eval()
+    print(f"test_dataloader: {test_dataloader}")
+
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(test_dataloader):
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+
+        valid_length = valid_length
+        label = label.long().to(device)
+
+        out = model(token_ids, valid_length, segment_ids)
+
+        test_eval = []
+        for i in out:  # out = model(token_ids, valid_length, segment_ids)
+            logits = i
+            logits = logits.detach().cpu().numpy()
+
+            if np.argmax(logits) == 0:
+                test_eval.append("공포")
+            elif np.argmax(logits) == 1:
+                test_eval.append("놀람")
+            elif np.argmax(logits) == 2:
+                test_eval.append("분노")
+            elif np.argmax(logits) == 3:
+                test_eval.append("슬픔")
+            elif np.argmax(logits) == 4:
+                test_eval.append("중립")
+            elif np.argmax(logits) == 5:
+                test_eval.append("행복")
+            elif np.argmax(logits) == 6:
+                test_eval.append("혐오")
+
+        print(data)
+        print(">> 입력하신 내용에서 " + test_eval[0] + "이(가) 느껴집니다.")
+        return test_eval[0]
